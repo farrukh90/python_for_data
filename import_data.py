930ac86de921025004c3a055f27e1b4987dc2cd4
farrukh90 <farrukhsadykov@gmail.com>
2020-01-01 23:59:46 -0600

    start

--- import_data.py
+++ import_data.py
@@ -1,15 +1,4 @@
 import pandas as pd
-# Creata my own header
-headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-# Get the data from URL and assign it to variable called url
-url = "https://archive.ics.uci.edu/ml/machine-learning-database/autos/imports-85.data"
-
-# Assign the data we downloaded above to a variable called df 
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(url)
-
-# Add the header we created for this data. 
-df.columns=headers
-
-# Returns more data above the objects
 print(df)
