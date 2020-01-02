aff34dfade98d1d7a545e12520716d09683013b1
farrukh90 <farrukhsadykov@gmail.com>
2020-01-01 22:31:05 -0600

    removed lines

--- import_data.py
+++ import_data.py
@@ -11,27 +11,7 @@ df = pd.read_csv(url)
 # Add the header we created for this data. 
 df.columns=headers
 
-# Print out  the data
-print(df.tail(10))
 
-# Export too "/tmp/somefile"
-df.to_csv("/tmp/somefile")
-
-# Gives data type for columns
-print(df.dtypes)
-
-# Returns more data above the objects
-print(df.describe())
-
-# Full summary statistics including Data for objects
-print(df.describe(include="all"))
-
-
-# Gives memory related information
-print(df.info())
-
-# Get 4 columns, and 10 bottom lines
-print(df[['a', 'b', 'c', 'd']].tail(10))
 
 somenumber = df['a']=df['a']+1
 print(somenumber)
