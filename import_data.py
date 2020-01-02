d73ec7a9fb5f812291639849ede76c62219804f8
farrukh90 <farrukhsadykov@gmail.com>
2020-01-01 21:08:41 -0600

    added header

--- import_data.py
+++ import_data.py
@@ -1,8 +1,7 @@
 import pandas as pd
+headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(url, header = None)
-
-headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-df.columns = headers
-df.head(5)
-print(df.columns)
+df.head()
+#print(df.tail(10))
