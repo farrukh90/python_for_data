9e299b5da722cf5227535beff8b0cf3a10a2f6c0
farrukh90 <farrukhsadykov@gmail.com>
2020-01-01 21:05:44 -0600

    added ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']

--- import_data.py
+++ import_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(url, header = None)
-print(df.tail(10))
+
+headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+df.columns = headers
+print(df.columns.tail(10))
