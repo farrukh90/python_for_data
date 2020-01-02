96fbb641c71e787fc1d3737f0432ca5c3f917bad
farrukh90 <farrukhsadykov@gmail.com>
2020-01-01 21:12:37 -0600

    added df.columns=headers

--- import_data.py
+++ import_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
-df = pd.read_csv(url, df.columns = headers)
+df = pd.read_csv(url)
+df.columns=headers
 print(df.tail(10))
