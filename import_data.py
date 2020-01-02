755250074dc2b1de43e2f4d6f85b6730287ab216
farrukh90 <farrukhsadykov@gmail.com>
2020-01-01 21:06:45 -0600

    added vdf.head(5)

--- import_data.py
+++ import_data.py
@@ -4,4 +4,5 @@ df = pd.read_csv(url, header = None)
 
 headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 df.columns = headers
+df.head(5)
 print(df.columns)
