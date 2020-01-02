73e85f65639d6ad687f748ffb69bde4078697629
farrukh90 <farrukhsadykov@gmail.com>
2020-01-02 00:07:29 -0600

    added headers

--- import_data.py
+++ import_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
-headers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"]url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(url)
 df.columns=headers
 print(df.tail(10))
+
+
